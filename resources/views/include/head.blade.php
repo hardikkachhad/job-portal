0f2380d86f94d6072839951adc4de3ba899e90f1
hardikcursor <cursorsoft776@gmail.com>
2025-04-01 17:29:22 +0530

    first commit

---
+++ resources/views/include/head.blade.php
@@ -0,0 +1,12 @@
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>CareerVibe | Find Best Jobs</title>
+	<meta name="description" content="" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=1, user-scalable=no" />
+	<meta name="HandheldFriendly" content="True" />
+	<meta name="pinterest" content="nopin" />
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/style.css') }}" />
+	<!-- Fav Icon -->
+	<link rel="shortcut icon" type="image/x-icon" href="#" />
+</head>
